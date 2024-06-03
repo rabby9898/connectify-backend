737f9b7ae206ea3dd9ed35ae67a428c673aa6c50
rabby9898 <mdfajlerabby00@gmail.com>
2024-06-03 08:39:34 +0600

    sign up api added

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,65 @@
+const userModel = require("../models/user.model");
+
 const signup = async (req, res) => {
+  try {
+    const { fullName, username, email, password } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        error: "Invalid email format",
+      });
+    }
+
+    const existUser = await userModel.findOne({ username });
+    if (existUser) {
+      return res.status(400).json({
+        error: "Username is already taken!",
+      });
+    }
+
+    const existEmail = await userModel.findOne({ email });
+    if (existEmail) {
+      return res.status(400).json({
+        error: "Email is already taken!",
+      });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, hash);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        followers: newUser.followers,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      res.status(400).json({
+        error: "Invalid User Data",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "Server error",
+    });
+  }
   res.json({
     data: "Sign up endpoint",
   });
